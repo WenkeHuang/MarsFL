584af96b25fb70184319b5f3283ab1ceeb4e2204
zekunshi <3167142424@qq.com>
2023-09-29 19:13:51 +0800

    update

--- domain_skew.sh
+++ domain_skew.sh
@@ -36,41 +36,58 @@
 #python main.py --task domain_skew --dataset Digits --method FedNova --device_id 7 --csv_log  --save_checkpoint &
 
 '''OfficeCaltech'''
-python main.py --task domain_skew --dataset OfficeCaltech \
-  --method FedAVG --device_id 0 --csv_log --csv_name 4_0.001 --save_checkpoint DATASET.parti_num 4 OPTIMIZER.local_train_lr 0.001 &
-
-python main.py --task domain_skew --dataset OfficeCaltech \
-  --method FedProx --device_id 1 --csv_log --csv_name 4_0.001_mu_0.01 --save_checkpoint DATASET.parti_num 4 OPTIMIZER.local_train_lr 0.001 Local.FedProxLocal.mu 0.01&
-
-python main.py --task domain_skew --dataset OfficeCaltech \
-  --method FedProx --device_id 2 --csv_log --csv_name 4_0.001_mu_0.001 --save_checkpoint DATASET.parti_num 4 OPTIMIZER.local_train_lr 0.001 Local.FedProxLocal.mu 0.001&
-
-python main.py --task domain_skew --dataset OfficeCaltech \
-  --method FedAVG --device_id 3 --csv_log --csv_name 4_0.005 --save_checkpoint DATASET.parti_num 4 OPTIMIZER.local_train_lr 0.005 &
-
-python main.py --task domain_skew --dataset OfficeCaltech \
-  --method FedProx --device_id 4 --csv_log --csv_name 4_0.005_mu_0.01 --save_checkpoint DATASET.parti_num 4 OPTIMIZER.local_train_lr 0.005 Local.FedProxLocal.mu 0.01&
-
-python main.py --task domain_skew --dataset OfficeCaltech \
-  --method FedProx --device_id 5 --csv_log --csv_name 4_0.005_mu_0.001 --save_checkpoint DATASET.parti_num 4 OPTIMIZER.local_train_lr 0.005 Local.FedProxLocal.mu 0.001&
+#python main.py --task domain_skew --dataset OfficeCaltech \
+#  --method FedAVG --device_id 0 --csv_log --csv_name 4_0.001 --save_checkpoint DATASET.parti_num 4 OPTIMIZER.local_train_lr 0.001 &
+#
+#python main.py --task domain_skew --dataset OfficeCaltech \
+#  --method FedProx --device_id 1 --csv_log --csv_name 4_0.001_mu_0.01 --save_checkpoint DATASET.parti_num 4 OPTIMIZER.local_train_lr 0.001 Local.FedProxLocal.mu 0.01&
+#
+#python main.py --task domain_skew --dataset OfficeCaltech \
+#  --method FedProx --device_id 2 --csv_log --csv_name 4_0.001_mu_0.001 --save_checkpoint DATASET.parti_num 4 OPTIMIZER.local_train_lr 0.001 Local.FedProxLocal.mu 0.001&
+#
+#python main.py --task domain_skew --dataset OfficeCaltech \
+#  --method FedAVG --device_id 3 --csv_log --csv_name 4_0.005 --save_checkpoint DATASET.parti_num 4 OPTIMIZER.local_train_lr 0.005 &
+#
+#python main.py --task domain_skew --dataset OfficeCaltech \
+#  --method FedProx --device_id 4 --csv_log --csv_name 4_0.005_mu_0.01 --save_checkpoint DATASET.parti_num 4 OPTIMIZER.local_train_lr 0.005 Local.FedProxLocal.mu 0.01&
+#
+#python main.py --task domain_skew --dataset OfficeCaltech \
+#  --method FedProx --device_id 5 --csv_log --csv_name 4_0.005_mu_0.001 --save_checkpoint DATASET.parti_num 4 OPTIMIZER.local_train_lr 0.005 Local.FedProxLocal.mu 0.001&
 
 '''OfficeHome'''
 python main.py --task domain_skew --dataset OfficeHome \
-  --method FedAVG --device_id 6 --csv_log --csv_name 4_0.001 --save_checkpoint DATASET.parti_num 4 OPTIMIZER.local_train_lr 0.001 &
+  --method FedAVG --device_id 0 --csv_log --csv_name 4_0.001 --save_checkpoint DATASET.domain_ratio 0.5 DATASET.parti_num 4 OPTIMIZER.local_train_lr 0.001 &
 
 python main.py --task domain_skew --dataset OfficeHome \
-  --method FedProx --device_id 7 --csv_log --csv_name 4_0.001_mu_0.01 --save_checkpoint DATASET.parti_num 4 OPTIMIZER.local_train_lr 0.001 Local.FedProxLocal.mu 0.01&
+  --method FedProx --device_id 1 --csv_log --csv_name 4_0.001_mu_0.01 --save_checkpoint DATASET.domain_ratio 0.5 DATASET.parti_num 4 OPTIMIZER.local_train_lr 0.001 Local.FedProxLocal.mu 0.01&
 
+python main.py --task domain_skew --dataset OfficeHome \
+  --method FedProx --device_id 2 --csv_log --csv_name 4_0.001_mu_0.001 --save_checkpoint DATASET.domain_ratio 0.5 DATASET.parti_num 4 OPTIMIZER.local_train_lr 0.001 Local.FedProxLocal.mu 0.001&
+
+python main.py --task domain_skew --dataset OfficeHome \
+  --method FedAVG --device_id 3 --csv_log --csv_name 4_0.005 --save_checkpoint DATASET.domain_ratio 0.5 DATASET.parti_num 4 OPTIMIZER.local_train_lr 0.005 &
+
+python main.py --task domain_skew --dataset OfficeHome \
+  --method FedProx --device_id 4 --csv_log --csv_name 4_0.005_mu_0.01 --save_checkpoint DATASET.domain_ratio 0.5 DATASET.parti_num 4 OPTIMIZER.local_train_lr 0.005 Local.FedProxLocal.mu 0.01&
+
+python main.py --task domain_skew --dataset OfficeHome \
+  --method FedProx --device_id 5 --csv_log --csv_name 4_0.005_mu_0.001 --save_checkpoint DATASET.domain_ratio 0.5 DATASET.parti_num 4 OPTIMIZER.local_train_lr 0.005 Local.FedProxLocal.mu 0.001&
+
+python main.py --task domain_skew --dataset OfficeHome \
+  --method FedAVG --device_id 6 --csv_log --csv_name 4_0.001 --save_checkpoint DATASET.domain_ratio 0.1 DATASET.parti_num 4 OPTIMIZER.local_train_lr 0.001 &
+
+python main.py --task domain_skew --dataset OfficeHome \
+  --method FedProx --device_id 7 --csv_log --csv_name 4_0.001_mu_0.01 --save_checkpoint DATASET.domain_ratio 0.1 DATASET.parti_num 4 OPTIMIZER.local_train_lr 0.001 Local.FedProxLocal.mu 0.01&
 wait
 python main.py --task domain_skew --dataset OfficeHome \
-  --method FedProx --device_id 0 --csv_log --csv_name 4_0.001_mu_0.001 --save_checkpoint DATASET.parti_num 4 OPTIMIZER.local_train_lr 0.001 Local.FedProxLocal.mu 0.001&
+  --method FedProx --device_id 0 --csv_log --csv_name 4_0.001_mu_0.001 --save_checkpoint DATASET.domain_ratio 0.1 DATASET.parti_num 4 OPTIMIZER.local_train_lr 0.001 Local.FedProxLocal.mu 0.001&
 
 python main.py --task domain_skew --dataset OfficeHome \
-  --method FedAVG --device_id 1 --csv_log --csv_name 4_0.005 --save_checkpoint DATASET.parti_num 4 OPTIMIZER.local_train_lr 0.005 &
+  --method FedAVG --device_id 1 --csv_log --csv_name 4_0.005 --save_checkpoint DATASET.domain_ratio 0.1 DATASET.parti_num 4 OPTIMIZER.local_train_lr 0.005 &
 
 python main.py --task domain_skew --dataset OfficeHome \
-  --method FedProx --device_id 2 --csv_log --csv_name 4_0.005_mu_0.01 --save_checkpoint DATASET.parti_num 4 OPTIMIZER.local_train_lr 0.005 Local.FedProxLocal.mu 0.01&
+  --method FedProx --device_id 2 --csv_log --csv_name 4_0.005_mu_0.01 --save_checkpoint DATASET.domain_ratio 0.1 DATASET.parti_num 4 OPTIMIZER.local_train_lr 0.005 Local.FedProxLocal.mu 0.01&
 
 python main.py --task domain_skew --dataset OfficeHome \
-  --method FedProx --device_id 3 --csv_log --csv_name 4_0.005_mu_0.001 --save_checkpoint DATASET.parti_num 4 OPTIMIZER.local_train_lr 0.005 Local.FedProxLocal.mu 0.001&
+  --method FedProx --device_id 3 --csv_log --csv_name 4_0.005_mu_0.001 --save_checkpoint DATASET.domain_ratio 0.1 DATASET.parti_num 4 OPTIMIZER.local_train_lr 0.005 Local.FedProxLocal.mu 0.001&
 
