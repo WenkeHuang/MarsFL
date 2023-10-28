109e11fee9b9482bebe8a5229b63380d7e83a1ee
zekunshi <3167142424@qq.com>
2023-10-28 15:25:41 +0800

    update

--- sh_file/backdoor_cifa10.sh
+++ sh_file/backdoor_cifa10.sh
@@ -2,9 +2,9 @@
 '''
 Cifar-10 0.5 0.2 base_backdoor
 '''
-python main.py --task label_skew --attack_type backdoor --dataset fl_cifar10 --method FedAVG --device_id 7 \
-       --csv_log --csv_name beta_0.5_bcr_0.2_MultiKrum --save_checkpoint  \
-       DATASET.parti_num 10 DATASET.beta 0.5 attack.backdoor.evils base_backdoor attack.bad_client_rate 0.2 FedAVG.global_method MultiKrumSever &
+#python main.py --task label_skew --attack_type backdoor --dataset fl_cifar10 --method FedAVG --device_id 7 \
+#       --csv_log --csv_name beta_0.5_bcr_0.2_MultiKrum --save_checkpoint  \
+#       DATASET.parti_num 10 DATASET.beta 0.5 attack.backdoor.evils base_backdoor attack.bad_client_rate 0.2 FedAVG.global_method MultiKrumSever &
 
 #python main.py --task label_skew --attack_type backdoor --dataset fl_cifar10 --method FedAVG --device_id 5 \
 #       --csv_log --csv_name beta_0.5_bcr_0.2_Bulyan --save_checkpoint  \
@@ -26,26 +26,26 @@ python main.py --task label_skew --attack_type backdoor --dataset fl_cifar10 --m
 #       --csv_log --csv_name beta_0.5_bcr_0.2_Rfa --save_checkpoint  \
 #       DATASET.parti_num 10 DATASET.beta 0.5 attack.backdoor.evils base_backdoor attack.bad_client_rate 0.2 FedAVG.global_method RfaSever &
 
-#python main.py --task label_skew --attack_type backdoor --dataset fl_cifar10 --method CRFL --device_id 5 \
+#python main.py --task label_skew --attack_type backdoor --dataset fl_cifar10 --method CRFL --device_id 0 \
 #       --csv_log --csv_name beta_0.5_bcr_0.2 --save_checkpoint \
 #       DATASET.parti_num 10 DATASET.beta 0.5 attack.backdoor.evils base_backdoor attack.bad_client_rate 0.2 &
 #
-#python main.py --task label_skew --attack_type backdoor --dataset fl_cifar10 --method RLR --device_id 5 \
+#python main.py --task label_skew --attack_type backdoor --dataset fl_cifar10 --method RLR --device_id 0 \
 #       --csv_log --csv_name beta_0.5_bcr_0.2 --save_checkpoint \
 #       DATASET.parti_num 10 DATASET.beta 0.5 attack.backdoor.evils base_backdoor attack.bad_client_rate 0.2 &
 
 '''
 Cifar-10 0.3 0.2 base_backdoor
 '''
-python main.py --task label_skew --attack_type backdoor --dataset fl_cifar10 --method FedAVG --device_id 7 \
-       --csv_log --csv_name beta_0.3_bcr_0.2_MultiKrum --save_checkpoint  \
-       DATASET.parti_num 10 DATASET.beta 0.3 attack.backdoor.evils base_backdoor attack.bad_client_rate 0.2 FedAVG.global_method MultiKrumSever &
+#python main.py --task label_skew --attack_type backdoor --dataset fl_cifar10 --method FedAVG --device_id 7 \
+#       --csv_log --csv_name beta_0.3_bcr_0.2_MultiKrum --save_checkpoint  \
+#       DATASET.parti_num 10 DATASET.beta 0.3 attack.backdoor.evils base_backdoor attack.bad_client_rate 0.2 FedAVG.global_method MultiKrumSever &
 
-python main.py --task label_skew --attack_type backdoor --dataset fl_cifar10 --method FedAVG --device_id 7 \
+python main.py --task label_skew --attack_type backdoor --dataset fl_cifar10 --method FedAVG --device_id 5 \
        --csv_log --csv_name beta_0.3_bcr_0.2_Bulyan --save_checkpoint  \
        DATASET.parti_num 10 DATASET.beta 0.3 attack.backdoor.evils base_backdoor attack.bad_client_rate 0.2 FedAVG.global_method BulyanSever &
 
-python main.py --task label_skew --attack_type backdoor --dataset fl_cifar10 --method FedAVG --device_id 7 \
+python main.py --task label_skew --attack_type backdoor --dataset fl_cifar10 --method FedAVG --device_id 5 \
        --csv_log --csv_name beta_0.3_bcr_0.2_TrimmedMean --save_checkpoint  \
        DATASET.parti_num 10 DATASET.beta 0.3 attack.backdoor.evils base_backdoor attack.bad_client_rate 0.2 FedAVG.global_method TrimmedMeanSever &
 
@@ -61,11 +61,11 @@ python main.py --task label_skew --attack_type backdoor --dataset fl_cifar10 --m
 #       --csv_log --csv_name beta_0.3_bcr_0.2_Rfa --save_checkpoint  \
 #       DATASET.parti_num 10 DATASET.beta 0.3 attack.backdoor.evils base_backdoor attack.bad_client_rate 0.2 FedAVG.global_method RfaSever &
 
-#python main.py --task label_skew --attack_type backdoor --dataset fl_cifar10 --method CRFL --device_id 5 \
+#python main.py --task label_skew --attack_type backdoor --dataset fl_cifar10 --method CRFL --device_id 2 \
 #       --csv_log --csv_name beta_0.3_bcr_0.2 --save_checkpoint \
 #       DATASET.parti_num 10 DATASET.beta 0.3 attack.backdoor.evils base_backdoor attack.bad_client_rate 0.2 &
 #
-#python main.py --task label_skew --attack_type backdoor --dataset fl_cifar10 --method RLR --device_id 5 \
+#python main.py --task label_skew --attack_type backdoor --dataset fl_cifar10 --method RLR --device_id 2 \
 #       --csv_log --csv_name beta_0.3_bcr_0.2 --save_checkpoint \
 #       DATASET.parti_num 10 DATASET.beta 0.3 attack.backdoor.evils base_backdoor attack.bad_client_rate 0.2 &
 
@@ -95,15 +95,15 @@ python main.py --task label_skew --attack_type backdoor --dataset fl_cifar10 --m
 #python main.py --task label_skew --attack_type backdoor --dataset fl_cifar10 --method FedAVG --device_id 5 \
 #       --csv_log --csv_name beta_0.5_bcr_0.2_Rfa --save_checkpoint  \
 #       DATASET.parti_num 10 DATASET.beta 0.5 attack.backdoor.evils semantic_backdoor attack.bad_client_rate 0.2 FedAVG.global_method RfaSever &
+
+#python main.py --task label_skew --attack_type backdoor --dataset fl_cifar10 --method CRFL --device_id 3 \
+#       --csv_log --csv_name beta_0.5_bcr_0.2 --save_checkpoint \
+#       DATASET.parti_num 10 DATASET.beta 0.5 attack.backdoor.evils semantic_backdoor attack.bad_client_rate 0.2 &
 #
-##python main.py --task label_skew --attack_type backdoor --dataset fl_cifar10 --method CRFL --device_id 5 \
-##       --csv_log --csv_name beta_0.5_bcr_0.2 --save_checkpoint \
-##       DATASET.parti_num 10 DATASET.beta 0.5 attack.backdoor.evils semantic_backdoor attack.bad_client_rate 0.2 &
-##
-##python main.py --task label_skew --attack_type backdoor --dataset fl_cifar10 --method RLR --device_id 5 \
-##       --csv_log --csv_name beta_0.5_bcr_0.2 --save_checkpoint \
-##       DATASET.parti_num 10 DATASET.beta 0.5 attack.backdoor.evils semantic_backdoor attack.bad_client_rate 0.2 &
-#
+#python main.py --task label_skew --attack_type backdoor --dataset fl_cifar10 --method RLR --device_id 3 \
+#       --csv_log --csv_name beta_0.5_bcr_0.2 --save_checkpoint \
+#       DATASET.parti_num 10 DATASET.beta 0.5 attack.backdoor.evils semantic_backdoor attack.bad_client_rate 0.2 &
+
 #'''
 #Cifar-10 0.3 0.2 semantic_backdoor
 #'''
@@ -130,12 +130,12 @@ python main.py --task label_skew --attack_type backdoor --dataset fl_cifar10 --m
 #python main.py --task label_skew --attack_type backdoor --dataset fl_cifar10 --method FedAVG --device_id 7 \
 #       --csv_log --csv_name beta_0.3_bcr_0.2_Rfa --save_checkpoint  \
 #       DATASET.parti_num 10 DATASET.beta 0.3 attack.backdoor.evils semantic_backdoor attack.bad_client_rate 0.2 FedAVG.global_method RfaSever &
+
+#python main.py --task label_skew --attack_type backdoor --dataset fl_cifar10 --method CRFL --device_id 4 \
+#       --csv_log --csv_name beta_0.3_bcr_0.2 --save_checkpoint \
+#       DATASET.parti_num 10 DATASET.beta 0.3 attack.backdoor.evils semantic_backdoor attack.bad_client_rate 0.2 &
 #
-##python main.py --task label_skew --attack_type backdoor --dataset fl_cifar10 --method CRFL --device_id 5 \
-##       --csv_log --csv_name beta_0.3_bcr_0.2 --save_checkpoint \
-##       DATASET.parti_num 10 DATASET.beta 0.3 attack.backdoor.evils semantic_backdoor attack.bad_client_rate 0.2 &
-##
-##python main.py --task label_skew --attack_type backdoor --dataset fl_cifar10 --method RLR --device_id 5 \
-##       --csv_log --csv_name beta_0.3_bcr_0.2 --save_checkpoint \
-##       DATASET.parti_num 10 DATASET.beta 0.3 attack.backdoor.evils semantic_backdoor attack.bad_client_rate 0.2 &
-#
+#python main.py --task label_skew --attack_type backdoor --dataset fl_cifar10 --method RLR --device_id 4 \
+#       --csv_log --csv_name beta_0.3_bcr_0.2 --save_checkpoint \
+#       DATASET.parti_num 10 DATASET.beta 0.3 attack.backdoor.evils semantic_backdoor attack.bad_client_rate 0.2 &
+
