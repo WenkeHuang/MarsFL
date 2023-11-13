5153c5be0bb3a838d4379191cdc54ff65fdb484a
Wenke Huang <591852399@qq.com>
2023-11-13 14:13:45 +0800

    update

--- main.py
+++ main.py
@@ -72,7 +72,6 @@ def main(args=None):
     args.conf_timestamp = str(datetime.datetime.now())
     args.conf_host = socket.gethostname()
 
-    # 整合方法的超参数
     cfg_dataset_path = os.path.join(config_path(), args.task, args.dataset, 'Default.yaml')
     cfg.merge_from_file(cfg_dataset_path)
 
@@ -107,32 +106,20 @@ def main(args=None):
 
         private_dataset.in_domain_list = in_domain_list  # 参与者能够从哪几个Domain中获取数据
 
-        # 先生成再删
-        # 随机采样
         temp_client_domain_list = ini_client_domain(args.rand_domain_select, private_dataset.domain_list, particial_cfg.DATASET.parti_num)
 
-        # 均分
-        # temp_client_domain_list = copy.deepcopy(private_dataset.domain_list) * (particial_cfg.DATASET.parti_num // len(private_dataset.domain_list))
-
-        # 是否用随机 如果不随机 那么均分 注意整除
-        # if args.use_random_domain:
-        #     temp_client_domain_list = ini_client_domain(args.rand_domain_select, private_dataset.domain_list, particial_cfg.DATASET.parti_num)
-        # else:
-        #     temp_client_domain_list = copy.deepcopy(private_dataset.domain_list) * (particial_cfg.DATASET.parti_num//len(private_dataset.domain_list))
 
         client_domain_list = []
         for i in range(len(temp_client_domain_list)):
             if temp_client_domain_list[i] != cfg[args.task].out_domain:
                 client_domain_list.append(temp_client_domain_list[i])
 
-        # 只用改一次 因为不是deepcopy
         particial_cfg.DATASET.parti_num = len(client_domain_list)
 
-        # cfg.freeze()
 
         private_dataset.client_domain_list = client_domain_list
         client_domain_list.append(cfg[args.task].out_domain)
-        # 参与者具体的Domain选择
+
         private_dataset.get_data_loaders(client_domain_list)
         private_dataset.out_train_loader = private_dataset.train_loaders.pop()
         client_domain_list.pop()
@@ -147,35 +134,25 @@ def main(args=None):
 
     if args.attack_type == 'byzantine':
 
-        # 数据集的信息
         if args.dataset in multi_domain_dataset_name:
-            # client_domain_list = ini_client_domain(args.rand_domain_select, private_dataset.domain_list, particial_cfg.DATASET.parti_num)
-            # private_dataset.get_data_loaders(client_domain_list)
             particial_cfg.attack.dataset_type = 'multi_domain'
 
         elif args.dataset in single_domain_dataset_name:
-            # private_dataset.get_data_loaders()
-            # client_domain_list = None
             particial_cfg.attack.dataset_type = 'single_domain'
 
-        # 攻击和未被攻击的客户端数量
         bad_scale = int(particial_cfg.DATASET.parti_num * particial_cfg['attack'].bad_client_rate)
         good_scale = particial_cfg.DATASET.parti_num - bad_scale
         client_type = np.repeat(True, good_scale).tolist() + (np.repeat(False, bad_scale)).tolist()
 
-        # 攻击类型是数据集攻击 那么修改数据集的内容
         attack_dataset(args, particial_cfg, private_dataset, client_type)
 
     elif args.attack_type == 'backdoor':
-        # 攻击和未被攻击的客户端数量
         bad_scale = int(particial_cfg.DATASET.parti_num * particial_cfg['attack'].bad_client_rate)
         good_scale = particial_cfg.DATASET.parti_num - bad_scale
         client_type = np.repeat(True, good_scale).tolist() + (np.repeat(False, bad_scale)).tolist()
 
-        # 攻击训练集
         backdoor_attack(args, particial_cfg, client_type, private_dataset, is_train=True)
 
-        # 攻击测试集用于测试专属指标
         backdoor_attack(args, particial_cfg, client_type, private_dataset, is_train=False)
 
     '''
@@ -190,9 +167,7 @@ def main(args=None):
     fed_method = get_fed_method(priv_backbones, client_domain_list, args, particial_cfg)
     assert args.structure in fed_method.COMPATIBILITY
 
-    # 将部分参数给方法
     if args.task == 'OOD':
-        # 加权数据集分配给method
         fed_method.train_eval_loaders = private_dataset.train_eval_loaders
 
         fed_method.test_loaders = private_dataset.test_loader
@@ -200,7 +175,6 @@ def main(args=None):
     if args.attack_type == 'byzantine':
         fed_method.client_type = client_type
 
-    # print(log_msg("CONFIG:\n{}".format(particial_cfg.dump()), "INFO"))
     if args.csv_name is None:
         setproctitle.setproctitle('{}_{}_{}'.format(args.method, args.task,args.dataset))
     else:
--- sh_file/lc.sh
+++
@@ -1,5 +0,0 @@
-python main.py --task label_skew --dataset fl_cifar10 --method Scaffold --device_id 5 --csv_log --csv_name beta_0.5 --save_checkpoint DATASET.beta 0.5 &
-python main.py --task label_skew --dataset fl_mnist --method Scaffold --device_id 6 --csv_log --csv_name beta_0.5 --save_checkpoint DATASET.beta 0.5 &
-python main.py --task label_skew --dataset fl_fashionmnist --method Scaffold --device_id 7 --csv_log --csv_name beta_0.5 --save_checkpoint DATASET.beta 0.5 &
-python main.py --task label_skew --dataset fl_cifar100 --method Scaffold --device_id 4 --csv_log --csv_name beta_0.5 --save_checkpoint DATASET.beta 0.5 &
-
